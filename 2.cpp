671367107ede27ab96fc33bc1deb9e3d419cc8fa
jamesmarfil99-coder <jamesmarfil99@gmail.com>
2025-08-22 18:21:18 +0800

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -5,17 +5,20 @@ using namespace std;
 
 int main() {
     string fullName;
-    int currentAge = 23;
+    int currentAge;
     int targetAge = 33;
-    int currentYear = 2025; 
+    int currentYear = 2025;
 
     cout << "Enter your full name: ";
     getline(cin, fullName);
 
+    cout << "Enter your current age: ";
+    cin >> currentAge;
+
     cout << "Hello " << fullName << ", You are " << currentAge << " years old!" << endl;
 
-    int yearTurnTarget = currentYear + (targetAge - currentAge);
-    cout << "You will turn " << targetAge << " in the year " << yearTurnTarget << endl;
+    int yearTurn60 = currentYear + (60 - currentAge);
+    cout << "You will turn 60 in the year " << yearTurn60 << endl;
 
     return 0;
 }
